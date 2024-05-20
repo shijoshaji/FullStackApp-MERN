15ce4bc3f4a061fa43102a12bd3666f33a1576e6
Shijo Shaji <jojovms@gmail.com>
2024-05-20 22:10:19 +0530

    createURL

--- server/src/controllers/shortURL-control.ts
+++ server/src/controllers/shortURL-control.ts
@@ -3,17 +3,22 @@ import { urlModel } from "../models/shortURL-model";
 
 
 export const createURL = async (req: express.Request, res: express.Response) => {
-    console.log("inside createURL");
+    console.log("inside:", createURL.name);
+    try {
+        let { fullURL } = req.body;
+        const urlFound = await urlModel.find({ fullURL: fullURL });
+        if (urlFound.length > 0) {
+            res.status(409).send({ "URL found": urlFound });
+        } else {
+            const shortURL = await urlModel.create({ fullURL });
+            res.status(201).send(shortURL);
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ err_msg: "Something went wrong", method_name: createURL.name });
+
+    }
 };
 
-export const getAllURL = async (req: express.Request, res: express.Response) => {
-    console.log("inside getAllURL");
-};
 
-export const getURL = async (req: express.Request, res: express.Response) => {
-    console.log("inside getURL");
-};
-
-export const deleteURL = async (req: express.Request, res: express.Response) => {
-    console.log("inside deleteURL");
-};
--- server/src/routes/shortURL-routes.ts
+++ server/src/routes/shortURL-routes.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { createURL, deleteURL, getAllURL, getURL } from "../controllers/shortURL-control";
+import { createURL, deleteURL, getAllURL, getURLByID } from "../controllers/shortURL-control";
 
 const router = express.Router();
 
 
 router.post("/shortURL", createURL);
 router.get("/shortURL", getAllURL);
-router.get("/shortURL/:id", getURL);
+router.get("/shortURL/:id", getURLByID);
 router.delete("/shortURL/:id", deleteURL);
 
 
